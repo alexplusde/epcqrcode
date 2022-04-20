4aaa305cda18a3dfbf0ff5179779167f09aa5461
Alexander Walther <aw@alexplus.de>
2022-04-20 12:33:57 +0200

    donation notice, version bump

--- pages/settings.php
+++ pages/settings.php
@@ -19,4 +19,22 @@ $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', 'Standard-Parameter', false);
 $fragment->setVar('body', $form->get(), false);
-echo $fragment->parse('core/page/section.php');
+?>
+
+<div class="row">
+    <div class="col-lg-8">
+        <?= $fragment->parse('core/page/section.php') ?>
+    </div>
+    <div class="col-lg-4">
+        <?php
+
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=epcqrcode"><img src="'.rex_url::addonAssets('epcqrcode', 'jetzt-spenden.svg').'" style="width: 100% max-width: 400px;"></a>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'info', false);
+$fragment->setVar('title', $this->i18n('epcqrcode_donate'), false);
+$fragment->setVar('body', '<p>' . $this->i18n('epcqrcode_info_donate') . '</p>' . $anchor, false);
+echo !rex_config::get("alexplusde", "donated") ?  $fragment->parse('core/page/section.php') : "";
+?>
+    </div>
+</div>
